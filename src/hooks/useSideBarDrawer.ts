bed933b033e860485a3b9e07a77786cea8bec44a
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-03 04:05:44 -0300

    Create hook to sidebar

---
+++ src/hooks/useSideBarDrawer.ts
@@ -0,0 +1,4 @@
+import { useContext } from "react";
+import { SideBarDrawerContext } from "../contexts/SideBarDrawerContext";
+
+export const useSideBarDrawer = () => useContext(SideBarDrawerContext);
