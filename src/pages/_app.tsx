832a06a6ad0c9f30b09edcfc849dccf6b0556d38
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-03 04:53:56 -0300

    Add miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SideBarDrawerProvider } from '../contexts/SideBarDrawerContext';
+import { makeServer } from '../services/mirage';
 import theme from '../styles/themes';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
