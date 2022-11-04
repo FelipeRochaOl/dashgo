f91d44d0b76ccdef2e2542e321e53ec7dfdaed38
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-04 13:40:10 -0300

    Add link in logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,16 +1,19 @@
 import { Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Text 
-      fontSize={['2xl', '3xl']}
-      fontWeight="bold"
-      letterSpacing="tight"
-      w="64"
-    >
-      dashgo
-      <Text as="span" color="pink.500" ml="1">.</Text>
-    </Text>
+    <Link href='/'>
+      <Text 
+        fontSize={['2xl', '3xl']}
+        fontWeight="bold"
+        letterSpacing="tight"
+        w="64"
+      >
+        dashgo
+        <Text as="span" color="pink.500" ml="1">.</Text>
+      </Text>
+    </Link>
   )
 }
 export default Logo;
