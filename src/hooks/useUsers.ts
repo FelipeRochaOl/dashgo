564e3e931cfff302115fc34391a3cfb738c50e80
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-05 05:26:31 -0300

    Change cacheTime to staleTime

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -48,7 +48,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 
 export function useUsers(page: number, options?: UseQueryOptions) {
   return useQuery(["users", page], () => getUsers(page), {
-    cacheTime: 1000 * 60 * 10,
+    staleTime: 1000 * 60 * 10,
     ...options,
   }) as UseQueryResult<GetUsersResponse, unknown>;
 }
