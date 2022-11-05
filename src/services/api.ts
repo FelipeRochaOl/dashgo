dfbfef251e17cf4f8b34524e19e023dc64bcf391
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-05 03:19:12 -0300

    Update api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.NEXT_PUBLIC_URL}/api`
+    : `${process.env.NEXT_PUBLIC_URL}/.netlify/functions`;
+
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL,
 });
